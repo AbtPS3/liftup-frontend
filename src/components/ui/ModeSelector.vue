7d9f1cd5674c68a18e16d49bb4da1a59dff1f9ac
Kizito Mrema <kizomanizo@gmail.com>
2024-01-17 15:08:24 +0300

    Changed top mode butons

--- src/components/ui/ModeSelector.vue
+++ src/components/ui/ModeSelector.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="mode-selector">
-    <a class="selector-link" :class="{ 'selected': selectedMode === 'clients' }" @click="selectMode('clients')">
+    <a class="selector-link" :class="{ 'left-tab selected': selectedMode === 'clients' }" @click="selectMode('clients')">
       {{ $t('shared.link.clients') }}
     </a>
-    <a class="selector-link" :class="{ 'selected': selectedMode === 'contacts' }" @click="selectMode('contacts')">
+    <a class="selector-link" :class="{ 'right-tab selected': selectedMode === 'contacts' }"
+      @click="selectMode('contacts')">
       {{ $t('shared.link.contacts') }}
     </a>
   </div>
@@ -26,7 +27,7 @@ const selectMode = (mode) => {
   position: relative;
   width: 100%;
   height: 40px;
-  background-color: var(--color-background);
+  /* background-color: var(--color-background); */
   display: grid;
   grid-template-columns: 1fr 1fr;
 }
@@ -38,12 +39,19 @@ const selectMode = (mode) => {
   /* border: 1px solid var(--color-background-mute); */
 }
 
+.mode-selector:has(selector-link:hover) .left-tab {
+  background: black;
+  font-weight: bold;
+}
+
 .selector-link:hover,
 .selector-link:visited {
-  background-color: var(--color-background-mute);
+  /* background-color: var(--color-background-mute); */
+  border-bottom: 2px solid var(--color-success-norm);
 }
 
 .selected {
-  background-color: var(--color-background-mute);
+  /* background-color: var(--color-background-mute); */
+  border-bottom: 2px solid var(--color-success-norm);
 }
 </style>
