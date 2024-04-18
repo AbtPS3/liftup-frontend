8e7038121be71b42e22c9a00c1b6361e2309acc7
Kizito Mrema <kizomanizo@gmail.com>
2024-04-18 11:25:59 +0300

    MINOR: changed assetsDir, removed leading slash

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   runtimeCompiler: false,
   build: {
     chunkSizeWarningLimit: 1600,
-    assetsDir: "/liftup/assets",
+    assetsDir: "liftup/assets",
     base: "/liftup/",
   },
 });
