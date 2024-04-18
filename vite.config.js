ed88f34b72cbca371d238dc85aeefe690bbd2bf9
Kizito Mrema <kizomanizo@gmail.com>
2024-04-18 10:56:47 +0300

    MINOR: Changed Vite BASE arg

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
   runtimeCompiler: false,
   build: {
     chunkSizeWarningLimit: 1600,
-    assetsDir: "liftup/assets",
+    // assetsDir: "liftup/assets",
+    base: "/",
   },
 });
